9c9695b3892a9ca1bab683f9240f60392b183aa5
wfnuser <wfnuser@126.com>
2015-10-24 22:57:49 +0800

    the only thing remains to do is to filter datar

--- p2p/p2p.js
+++ p2p/p2p.js
@@ -1,11 +1,12 @@
+var select_state = [0,0,0];
 var p2p_filter = new Vue({
   el: '#p2p_filter',
   data: {
     state:[false,false,false],
     select_titles: [
-      { child_title: '投资金额',child_content:['1','2','3'],ifdisplay: false, },
-      { child_title: '投资周期',child_content:['4','6','8'],ifdisplay: false, },
-      { child_title: '预期收益',child_content:['7','9','0'],ifdisplay: false, }
+      { child_title: '投资金额',child_title_bk: '投资金额',child_content:['1','2','3'],ifdisplay: false,index:0 },
+      { child_title: '投资周期',child_title_bk: '投资金额',child_content:['4','6','8'],ifdisplay: false,index:1 },
+      { child_title: '预期收益',child_title_bk: '投资金额',child_content:['7','9','0'],ifdisplay: false,index:2 }
     ]
   },
   methods: {
@@ -24,6 +25,14 @@ var p2p_filter = new Vue({
         p2p_filter.select_titles[0].ifdisplay = false;
         p2p_filter.select_titles[1].ifdisplay = false;
         p2p_filter.select_titles[2].ifdisplay = false;
+    },
+    select: function (selecttype,filtertype,filtervalue,item) {
+      if(selecttype==0){
+        item.child_title = item.child_title_bk;
+      }
+      else{
+        item.child_title = filtervalue;
+      }
     }
   },
 })
